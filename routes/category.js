177684080e66d0be72539b1b8c07b7429bae00fa
jiekai <jiekai22s@forwardschool.edu.my>
2023-08-28 09:07:08 +0800

    u

--- routes/category.js
+++ routes/category.js
@@ -6,7 +6,7 @@ const Category = require("../models/category");
 
 router.get("/", async (req, res) => {
   try {
-    res.status(200).send(await Category.find().populate("tasks"));
+    res.status(200).send(await Category.find().populate("task"));
   } catch (error) {
     res.status(400).send("Failed to update the category");
   }
